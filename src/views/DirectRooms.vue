81623fab3b39eced67de94caedf71c2233b77e88
Daniel <danieruone@gmail.com>
2022-05-12 21:43:58 -0500

    Use store chat rooms

--- src/views/DirectRooms.vue
+++ src/views/DirectRooms.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="directRoomsContainer">
-    <ChatPreview v-for="(dm, idx) in mockDM" :key="idx" v-bind="dm" />
+    <ChatPreview v-for="(dm, idx) in directChatRooms" :key="idx" v-bind="dm" />
   </div>
 </template>
 
 <script>
-const mockDM = [
-  {
-    name: 'Daniel',
-    online: true,
-    lastMessage: 'Que más!',
-    time: '09:00',
-    id: 'abc123',
-  },
-  {
-    name: 'Carlos',
-    online: false,
-    lastMessage: 'Cómo sería',
-    time: '09:00',
-    id: 'abc124',
-  },
-  {
-    name: 'Alfredo',
-    online: true,
-    lastMessage: 'Vamos a jugar o qué',
-    time: '09:00',
-    id: 'abc125',
-  },
-];
-
 import ChatPreview from '@/components/ChatPreview.vue';
 export default {
   name: 'DirectRooms',
   components: { ChatPreview },
   data() {
     return {
-      mockDM,
+      directChatRooms: this.$store.state.directChatRooms,
     };
   },
 };
