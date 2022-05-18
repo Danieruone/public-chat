cb789748e6d973a0c3cc450c599f8b79f5202f1d
Daniel <danieruone@gmail.com>
2022-05-17 20:15:09 -0500

    Add chat styles and functions

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="messageContainer">
+    <div></div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatPreview',
+  props: {
+    name: String,
+    message: String,
+    id: String,
+  },
+};
+</script>
+
+<style scoped>
+.messageContainer {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.messageContainer div {
+  width: 20px;
+  height: 20px;
+  background-color: #ffc301;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+
+.messageContainer p {
+  background-color: #01cef9;
+  border-radius: 20px;
+  color: white;
+  padding: 10px;
+  margin: 0;
+}
+</style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="chatContainer">
-    <h3>{{ chatName }}</h3>
+    <div class="header">
+      <router-link to="/rooms/public">
+        <img src="@/assets/left-arrow.png"
+      /></router-link>
+      <h3>{{ chatName }}</h3>
+    </div>
+
     <div class="messagesContainer">
-      <p>Message: {{ message }}</p>
+      <ChatMessage
+        v-for="(chatMessage, idx) in chatMessages"
+        :key="idx"
+        v-bind="chatMessage"
+      />
     </div>
     <div class="inputContainer">
       <input type="text" placeholder="Type your message..." v-model="message" />
-      <button @click="sendMessage">Send</button>
+      <div @click="sendMessage" class="sendButton">
+        <img src="@/assets/paper-plane.png" />
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +27,17 @@
 import { onMounted, onBeforeUnmount, ref } from 'vue';
 import { useStore } from 'vuex';
 import { useRoute } from 'vue-router';
+
+// components
+import ChatMessage from '@/components/ChatMessage.vue';
+
 export default {
+  components: { ChatMessage },
+  computed: {
+    chatMessages() {
+      return this.$store.state.chatModule.chatMessages;
+    },
+  },
   setup() {
     const store = useStore();
     const route = useRoute();
@@ -61,6 +83,7 @@ export default {
             },
           })
         );
+      message.value = '';
     };
 
     return {
@@ -78,24 +101,66 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   height: 99vh;
-  border: solid 1px red;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+  justify-content: center;
+  background-color: white;
+}
+
+.header img {
+  width: 20px;
+  height: 20px;
+  margin-right: 7rem;
+  position: absolute;
+  left: 10px;
+  top: 1.3rem;
 }
 
 .messagesContainer {
-  border: 1px solid green;
   height: 100%;
+  background-color: #f5f7fb;
+  padding: 1rem;
 }
 
 .inputContainer {
   height: 5rem;
-  border: solid 1px purple;
+  background-color: white;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
 }
 
 .inputContainer input {
   padding: 10px 30px;
   margin-right: 10px;
+  font-size: 1rem;
+  border-radius: 30px;
+  border: none;
+  background-color: #efefef;
+}
+
+.sendButton {
+  width: 40px;
+  height: 40px;
+  padding: 2px;
+  border-radius: 50%;
+  background-color: #dddddd;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.sendButton img {
+  width: 25px;
+  height: 25px;
 }
 </style>
